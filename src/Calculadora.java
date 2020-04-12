80379bbfe5653d3da09b119d072cf1eece3017cb
Andrijasevich Federico <fede_andrijasevich@hotmail.com>
2020-04-11 22:18:56 -0300

    Modificacion de funcion Sumar()

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,7 +5,12 @@ public class Calculadora {
 	Scanner teclado = new Scanner(System.in);
 	
 	public void Sumar() {
-		
+		System.out.println("Ingrese el primer numero a sumar: ");
+		Integer numero1 = teclado.nextInt();
+		System.out.println("Ingrese el segundo numero a sumar: ");
+		Integer numero2= teclado.nextInt();
+		Integer resultado = numero1 + numero2;
+		System.out.println(numero1 + " + " + numero2 + "= " + resultado);
 	}
 }
 	
