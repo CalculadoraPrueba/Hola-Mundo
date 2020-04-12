2d897dc3f20b2b22dc12ad285d4cda9115fc0af6
Andrijasevich Federico <fede_andrijasevich@hotmail.com>
2020-04-12 15:11:33 -0300

    Se elimino en HolaMundo opciones de funciones que todavia no se crearon

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -7,10 +7,6 @@ public class HolaMundo {
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("1. Sumar");
 		System.out.println("2. Restar");
-		System.out.println("3. Multiplicar");
-		System.out.println("4. Dividir");
-		System.out.println("5. Potencia");
-		System.out.println("6. Factorial");
 		System.out.println("0. Terminar");
 		Integer opcion = teclado.nextInt();
 		
@@ -25,33 +21,13 @@ public class HolaMundo {
 			case 2: 
 				calculo.Restar();
 				break;
-				
-			case 3:
-				calculo.Multiplicar();
-				break;
-				
-			case 4: 
-				calculo.Dividir();
-				break;
-			
-			case 5: 
-				calculo.Potencia();
-				break;
-				
-			case 6:
-				calculo.Factorial();
-				break;
-			
+	
 			default:
 				System.out.println("No existe esa opcion vuelva a ingresar");
 			}
 			
 			System.out.println("1. Sumar");
 			System.out.println("2. Restar");
-			System.out.println("3. Multiplicar");
-			System.out.println("4. Dividir");
-			System.out.println("5. Potencia");
-			System.out.println("6. Factorial");
 			System.out.println("0. Terminar");
 			opcion = teclado.nextInt();
 		}
