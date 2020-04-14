97bbf2b936166da1c4299081e530d6994a3bde4e
Candela Andrijasevich <candelaandrijasevich@gmail.com>
2020-04-14 00:32:52 -0300

    Desarollamos funcion Multiplicar()

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -20,12 +20,17 @@ public class Calculadora {
 		Integer numero1 = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero: ");
 		Integer numero2 = teclado.nextInt();
-		Integer resultado = numero1 + numero2;
+		Integer resultado = numero1 - numero2;
 		System.out.println(numero1 + " - " + numero2 + "= " + resultado);
 	}
 	
 	public void Multiplicar() {
-		
+            System.out.println("Ingrese un numero");
+            Integer numero1 = teclado.nextInt();
+            System.out.println("Ingrese el segundo numero: ");
+            Integer numero2 = teclado.nextInt();
+            Integer resultado = numero1 * numero2;
+            System.out.println(numero1 + "*"+ numero2 + "=" + resultado);
 	}
 }
 	
