a82f1198d40080550695338ac4755d6cbc739597
Andrijasevich Federico <fede_andrijasevich@hotmail.com>
2020-04-14 17:34:51 -0300

    Se elimino opcion de dividir

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -8,7 +8,6 @@ public class HolaMundo {
 		System.out.println("1. Sumar");
 		System.out.println("2. Restar");
 		System.out.println("3. Multiplicar");
-		System.out.println("4. Dividir");
 		System.out.println("0. Terminar");
 		Integer opcion = teclado.nextInt();
 		
@@ -27,17 +26,12 @@ public class HolaMundo {
 			case 3: 
 				calculo.Multiplicar();
 				break;
-				
-			case 4: 
-				calculo.Dividir();
-				break;
 			
 			}
 			
 			System.out.println("1. Sumar");
 			System.out.println("2. Restar");
 			System.out.println("3. Multiplicar");
-			System.out.println("4. Dividir");
 			System.out.println("0. Terminar");
 			opcion = teclado.nextInt();
 		}
